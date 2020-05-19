94ec035f71a9ef980c6a846d6eb7c3b0a411f01f
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-05-19 21:33:32 +0800

    0519

---
+++ CardGame/Assets/Scrip/BookCard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BookCard : MonoBehaviour
+{
+    /// <summary>
+    /// 卡排圖鑑的編號
+    /// </summary>
+    public int index;
+
+    // 原本寫法 : 透過尋找
+    // public DeckMamager deckMamager;
+
+    private void Start()
+    {
+        // 原本寫法，透過尋找
+        // deckManager = FindObjectOfType<DeckMamager>();
+
+        // 取得按鈕.點擊.添加監聽器(方法)
+        GetComponent<Button>().onClick.AddListener(ChooseBookCard);
+    }
+
+    /// <summary>
+    /// 選擇圖鑑內的卡牌
+    /// </summary>
+    private void ChooseBookCard()
+    {
+        print("選取的圖鑑編號為 :" + index);
+
+        DeckMamager.instance.AddCard(index);
+    }
+}
